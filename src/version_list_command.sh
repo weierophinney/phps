a6becdc19a3b63c8aebecb7a90e7f8932a65b655
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-10-13 17:28:27 -0500

    refactor: rewrite "version list" command in bash

--- src/version_list_command.sh
+++ src/version_list_command.sh
@@ -1,11 +1,14 @@
-# shellcheck disable=SC2215
+# shellcheck disable=SC2215,SC2168
 name: list
 help: |-
   List supported PHP versions.
 
   Provides a list of installed PHP versions to choose from.
 ---
-echo "# this file is located in 'src/version_list_command.sh'"
-echo "# code for 'phps version list' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+local versions
+local version
+
+mapfile -t versions < <(alternatives_get_supported_versions | sort)
+for version in "${versions[@]}"; do
+    echo "- ${version}"
+done
