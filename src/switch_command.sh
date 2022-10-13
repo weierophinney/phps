e460ef261b2d371099d1ac344dd72a42b666103d
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-10-13 16:43:17 -0500

    refactor: rewrites "switch" command in bash

--- src/switch_command.sh
+++ src/switch_command.sh
@@ -1,4 +1,4 @@
-# shellcheck disable=SC2215
+# shellcheck disable=SC2215,SC2168,SC2154
 name: switch
 help: |-
   Choose which PHP version to use by default.
@@ -8,7 +8,27 @@ help: |-
 args:
 - import: src/flags/version_arg.yml
 ---
-echo "# this file is located in 'src/switch_command.sh'"
-echo "# code for 'phps switch' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+local version="${args[version]}"
+local supported_versions
+local command
+
+if [[ "${version}" == "auto" ]]; then
+    version="$(alternatives_get_best_version)"
+fi
+
+supported_versions="$(alternatives_get_supported_versions)"
+if [[ "${supported_versions}" != *"${version}"* ]]; then
+    red "Unsupported version; must be one of:"
+    red "${supported_versions}"
+    exit 1
+else
+    command="$(alternatives_command)"
+    if $command --set php "/usr/bin/php${version}"; then
+        green "Now using PHP version ${version}"
+        green "If you have previously aliased to a specific version, run:"
+        echo "    unalias php"
+    else
+        red "Unable to switch PHP version"
+        exit 1
+    fi
+fi
